8e6fa33fc5e66996f88ddf60350733db3ae978f0
name <varunbrahmane30@gmail.com>
2021-08-20 20:30:14 +0530

    [varun] added: Degree Conversion problem using function.

---
+++ Repetation/Function/DegreeConversion.js
@@ -0,0 +1,15 @@
+// function declaration
+function DegCToDegF(value)
+{
+    celcius=(value*(9/5)+32);
+    console.log("Deg C -> Deg F:"+celcius);
+}
+function DegFToDegC(val)
+{
+    Feranhite=(val-32)*(5/9)
+    console.log("Deg F -> Deg C:"+Feranhite);
+}
+
+// function call
+DegCToDegF(25);
+DegFToDegC(45);
