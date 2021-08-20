8b2a1bc8babec93a8388ce51c1c4279a0142776b
name <varunbrahmane30@gmail.com>
2021-08-20 18:58:00 +0530

    [varun] added: get a result of head and tails.

---
+++ Selection/IfElseProb/FlipCoin.js
@@ -0,0 +1,12 @@
+// get a random number as input.
+let flipCoinResult = Math.floor(Math.random()*10) % 2;
+
+// condition check 
+if(flipCoinResult==0)
+{
+    console.log("You've get Heads");
+}
+else
+{
+    console.log("You've get Tails");
+}
